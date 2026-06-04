6d3b25514801250b104cf8cfa9b60ce70e35d5da
Kiet Vuong <kiet.vuong@growwithtreehouse.ca>
2026-06-04 13:00:32 -0400

    removed prerenderer

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePrerenderPlugin } from 'vite-prerender-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    vitePrerenderPlugin({
-      renderTarget: '#root',
-      prerenderScript: './src/prerender.tsx',
-    }),
   ],
   base: '/sim-card-store/',
 })
